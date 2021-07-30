4556ba03da445be760fa52dde3376921876d8ee3
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-07-31 02:25:41 +0700

    4.State

--- App.js
+++ App.js
@@ -3,13 +3,13 @@ import React from "react";
 import { View } from "react-native";
 
 //Import Screen
-import Props from "./src/screens/props";
+import State from "./src/screens/state";
 
 export default function App() {
   return (
     <View style={{ marginTop: 200 }}>
       <StatusBar />
-      <Props />
+      <State />
     </View>
   );
 }
