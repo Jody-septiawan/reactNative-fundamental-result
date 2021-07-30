578aff3efd7d2f3db6dbcf7a645a8bdffbde5dea
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-07-31 01:00:06 +0700

    1.Component

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View } from "react-native";
+
+//Import Component
+import Content from "./src/components/content";
+import Header from "./src/components/header";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar />
+      <Header />
+      <Content />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
